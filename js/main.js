a4d54d48a3e3d9165fbd5e25f1d3d8900c7d7fe1
Vladislav <infohunterr@gmail.com>
2020-10-09 17:20:25 +0600

    ghgh

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ function toggleModal() {
 }
 
 new WOW().init();
+
+const modalDialog = document.querySelector("#modal-dialog")
+let class1 = 'wow animate__animated';
+let class2 = 'animate__fadeInUp';
+const classes = class1.split(' ').concat(class2);
+/* element.classList.add(...classes); */
+
+modalDialog.addEventListener('click', function (event) {
+modalDialog.classList.remove(...classes);
+modalDialog.classList.add(...classes);
+});
+
+/* const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener('click', function (event) {
+modal.classList.add("modal-active");
+});
+
+close.addEventListener('click', function (event) {
+modal.classList.remove("modal-active");
+});
+
+function toggleModal() {
+  modal.classList.toggle("modal-active");
+} */
